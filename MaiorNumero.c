ec160c0d2cc75e1e894d71b6884a3dac3a84b9cd
Felipe Bellanda <felipebellanda0808@gmail.com>
2026-08-18 09:17:53 -0300

    Add files via upload

---
+++ MaiorNumero.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int a, b, c, maior, maior_temp;
+	printf("Insira tres valores: ");
+	scanf("%d %d %d", &a, &b , &c);
+	
+	maior_temp = ((a+b + abs(a-b))/2);
+	maior = (((maior_temp+c) + abs(maior_temp-c))/2);
+	printf("O maior entre %d|%d|%d = %d", a, b, c, maior);
+	
+	return 0;
+}
